e100690509906a0c4acbc32274e08bcc0ec45259
jeongyeob <sjy20000@gmail.com>
2022-08-07 13:10:36 +0900

    서비스 워커 경로 오류 수정 확장프로그램 아이콘 클릭시 인벤 몬스터 페이지로 이동

--- service-worker/background.js
+++ service-worker/background.js
@@ -1,14 +1,28 @@
 /**
  * Extension이 설치될때
  * 
- * details: object
- *  id: string optional
- *  previousVersion: string optional
- *  reason: OnInstalledReason
- *      "install"
- *      "update"
- *      "chrome_update"
- *      "shared_module_update"
+ * * details: object
+ * *  id: string optional
+ * *  previousVersion: string optional
+ * *  reason: OnInstalledReason
+ * *      "install"
+ * *      "update"
+ * *      "chrome_update"
+ * *      "shared_module_update"
  */
 chrome.runtime.onInstalled.addListener((details) => {
+});
+
+
+/**
+ * 확장 프로그램 아이콘이 클릭 됬을때
+ * 
+ * * 몬스터 리스트 페이지로 이동한다.
+ * ! popup이 설정되면 작동 하지 않는다.
+ */
+chrome.action.onClicked.addListener((tab) => {
+    let url = "https://mhf.inven.co.kr/dataninfo/mhr/monster/";
+    if(tab.url != url){
+        chrome.tabs.update(tab.id, {url});
+    }
 });
